9ad4f3162f6b7d589642f3dec98fa0644e78a163
Akshit Raj Singh <akshitraj.rkt@gmail.com>
2025-01-30 17:08:21 +0530

    staticfiles

--- areas/areas/settings.py
+++ areas/areas/settings.py
@@ -137,5 +137,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS= os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'staticfiles/'),
+],
 STATIC_ROOT = BASE_DIR / 'staticfiles'
