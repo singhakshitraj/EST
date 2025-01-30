35f558bab66ec741478c110d77a839029a04e327
Akshit Raj Singh <akshitraj.rkt@gmail.com>
2025-01-30 16:18:13 +0530

    try-again-3

--- areas/areas/wsgi.py
+++ areas/areas/wsgi.py
@@ -14,6 +14,6 @@ print("Python Path:", sys.path)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'areas.areas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'areas.settings')
 
 application = get_wsgi_application()
